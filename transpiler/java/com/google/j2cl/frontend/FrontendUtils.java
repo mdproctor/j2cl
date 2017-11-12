4bdc10a801d15cf7d30fe371de664af44ac3d084
Colin Alworth <colin@colinalworth.com>
2017-11-11 19:12:37 -0600

    Allow output to be generated in sources that don't yet exist

--- transpiler/java/com/google/j2cl/frontend/FrontendUtils.java
+++ transpiler/java/com/google/j2cl/frontend/FrontendUtils.java
@@ -132,6 +132,7 @@ public class FrontendUtils {
 
     // Ensures that we will not fail if the zip already exists.
     outputPath.toFile().delete();
+    outputPath.toFile().getParentFile().mkdirs();
 
     try {
       return FileSystems.newFileSystem(
